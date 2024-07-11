d2dfc9199873d2e944e4168474e1d0ad50995c34
MatrixMyRIP <lolariana31@gmail.com>
2024-07-12 02:55:51 +0300

    Add files via upload

--- landing/views.py
+++ landing/views.py
@@ -38,16 +38,3 @@ class TemplView(View):
             # return JsonResponse(form.cleaned_data)
             return JsonResponse(data=form.cleaned_data, json_dumps_params={"indent": 4, "ensure_ascii": False})
         return render(request, 'landing/index.html', context={"form": form})
-
-
-def ip_user(self, request):
-    my_text = self.cleaned_data['my_text']
-    my_email = self.cleaned_data['my_email']
-    my_message = self.cleaned_data['my_message']
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]  # Получение IP
-    else:
-        ip = request.META.get('REMOTE_ADDR')  # Получение IP
-
-    user_agent = request.META.get('HTTP_USER_AGENT')
